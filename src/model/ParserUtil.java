8b54c510baedaf4a691e26fc3430008d4ce9f497
ShayRubach <shayrubach@gmail.com>
2018-03-06 12:10:54 +0200

    finished the indexing algorithm and upload to db (too slow). added some stop list words. remove blank chars at db.

--- src/model/ParserUtil.java
+++ src/model/ParserUtil.java
@@ -3,7 +3,9 @@ package model;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -31,18 +33,42 @@ public class ParserUtil {
 
     }
 
-    public void indexFile(int docId, String fileName, DatabaseUtil db) throws FileNotFoundException {
-        //TODO: use the db to upload the index table
+    public void indexFile(int docId, String fileName, DatabaseUtil db) throws FileNotFoundException, SQLException {
         String line;
-
+        String[] words;
+        HashMap<String,Integer> appearances = new HashMap<>();
         Scanner itr = new Scanner(new File(db.getLocalStoragePath() + "/"+ fileName));
         while(itr.hasNextLine()){
             line = itr.nextLine();
-
             //remove all punctuations but the ' character :
             line = line.replaceAll("(?=[^a-zA-Z0-9])([^'])", " ").toLowerCase();
+            words = line.split(" ");
+            mapWords(words,line,appearances);
+        }
+        for (String key : appearances.keySet() ) {
+            db.pStmt = db.getConn().prepareStatement(QueryUtil.INSERT_NEW_INDEX_FILE);
+            db.pStmt.setString(1,key);
+            db.pStmt.setInt(2,docId);
+            db.pStmt.setInt(3,appearances.get(key));
+            db.pStmt.execute();
+        }
+
+        //TODO: use the db to upload file to the index table
 
 
+    }
+
+    private void mapWords(String[] words, String line,HashMap<String,Integer> appearances) {
+
+        for(String s : words){
+            if(!appearances.containsKey(s))
+                appearances.put(s,1);
+            else
+                appearances.put(s,appearances.get(s)+1);
+        }
+
+        if(appearances.containsKey("")){
+            appearances.remove("");
         }
 
     }
